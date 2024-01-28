c635d55c72dabb07adc1486938c7a2d59d0efa6e
hersh311 <61861759+hersh311@users.noreply.github.com>
2024-01-28 14:07:37 +0530

    goofy meter

---
+++ projectmakemelaugh/Assets/GoofyScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoofyScript : MonoBehaviour
+{
+    private float rotationValue = 0;
+    public float scaledValue = 0.5f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.RotateAround(transform.parent.position, transform.parent.forward, rotationValue);
+        rotationValue = Mathf.Clamp(rotationValue, -0.8f, 0.15f);
+        scaledValue = Mathf.Clamp01(scaledValue);
+        rotationValue = Mathf.Lerp(-0.8f, 0.15f, scaledValue);
+        transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y, rotationValue, transform.localRotation.w);
+    }
+
+}
